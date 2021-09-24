2bc1c87d214c25cce0bb23d49e423fc9d22857ad
Leandro Segovia <ldlsegovia@gmail.com>
2021-09-24 13:10:27 -0300

    feat(): add linter formatter

--- lib/simplecov_linter_formatter.rb
+++ lib/simplecov_linter_formatter.rb
@@ -8,3 +8,30 @@ require 'simplecov_linter_formatter/exporters/json_result_exporter'
 module SimpleCovLinterFormatter
   FILENAME = 'coverage.linter.txt'
 end
+
+module SimpleCov
+  module Formatter
+    class LinterFormatter
+      def format(simplecov_result)
+        text_lines = format_result(simplecov_result)
+        hash_result = format_text_lines(text_lines)
+        export_to_json(hash_result)
+        nil
+      end
+
+      private
+
+      def format_result(simplecov_result)
+        SimpleCovLinterFormatter::ResultFormatter.new(simplecov_result).format
+      end
+
+      def format_text_lines(text_lines)
+        SimpleCovLinterFormatter::TextLinesFormatter.new(text_lines).format
+      end
+
+      def export_to_json(hash_result)
+        SimpleCovLinterFormatter::JsonResultExporter.new(hash_result).export
+      end
+    end
+  end
+end
--- spec/simplecov_linter_formatter_spec.rb
+++ spec/simplecov_linter_formatter_spec.rb
@@ -1,7 +1,48 @@
 require "spec_helper"
 
-describe SimpleCovLinterFormatter do
-  it "has a version number" do
-    expect(SimpleCovLinterFormatter::VERSION).not_to be nil
+describe SimpleCov::Formatter::LinterFormatter do
+  let(:simplecov_result) { double }
+  let(:text_lines) { double }
+  let(:hash_result) { double }
+
+  let(:result_formatter) { double(format: text_lines) }
+  let(:text_lines_formatter) { double(format: hash_result) }
+  let(:json_result_exporter) { double(export: true) }
+
+  def format
+    described_class.new.format(simplecov_result)
+  end
+
+  before do
+    allow(SimpleCovLinterFormatter::ResultFormatter)
+      .to receive(:new).and_return(result_formatter)
+
+    allow(SimpleCovLinterFormatter::TextLinesFormatter)
+      .to receive(:new).and_return(text_lines_formatter)
+
+    allow(SimpleCovLinterFormatter::JsonResultExporter)
+      .to receive(:new).and_return(json_result_exporter)
+  end
+
+  it { expect(format).to eq(nil) }
+
+  it do
+    format
+
+    expect(SimpleCovLinterFormatter::ResultFormatter).to have_received(:new)
+      .with(simplecov_result).once
+
+    expect(SimpleCovLinterFormatter::TextLinesFormatter).to have_received(:new)
+      .with(text_lines).once
+
+    expect(SimpleCovLinterFormatter::JsonResultExporter).to have_received(:new)
+      .with(hash_result).once
+  end
+
+  it do
+    format
+    expect(result_formatter).to have_received(:format).with(no_args).once
+    expect(text_lines_formatter).to have_received(:format).with(no_args).once
+    expect(json_result_exporter).to have_received(:export).with(no_args).once
   end
 end
