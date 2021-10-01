f3c366f7ba0bdc60135688504740add7b7989ee7
Leandro Segovia <ldlsegovia@gmail.com>
2021-10-01 20:15:10 -0300

    feat(text-lines-filter): use git diff if reviewdog is not installed

--- spec/lib/text_lines_filter_spec.rb
+++ spec/lib/text_lines_filter_spec.rb
@@ -1,6 +1,8 @@
 require "spec_helper"
 
 describe SimpleCovLinterFormatter::TextLinesFilter do
+  let(:use_reviewdog) { true }
+
   let(:lines) do
     %w{
       line1
@@ -8,7 +10,7 @@ describe SimpleCovLinterFormatter::TextLinesFilter do
     }
   end
 
-  let(:filtered_lines) { "filtered_line1\nfiltered_line2" }
+  let(:diff_result) { "filtered_line1\nfiltered_line2" }
 
   let(:expected_result) do
     %w{
@@ -18,7 +20,8 @@ describe SimpleCovLinterFormatter::TextLinesFilter do
   end
 
   before do
-    allow_any_instance_of(described_class).to receive(:`).and_return(filtered_lines)
+    allow_any_instance_of(described_class).to receive(:system).and_return(use_reviewdog)
+    allow_any_instance_of(described_class).to receive(:`).and_return(diff_result)
   end
 
   def filter
@@ -29,7 +32,7 @@ describe SimpleCovLinterFormatter::TextLinesFilter do
 
   it do
     expect_any_instance_of(described_class).to receive(:`)
-      .with("echo \"line1\\nline2\" | reviewdog -efm=\"%f:%l:%c:%m\" -diff=\"git diff\"")
+      .with("echo \"line1\nline2\" | reviewdog -efm=\"%f:%l:%c:%m\" -diff=\"git diff\"")
       .once
 
     filter
@@ -40,5 +43,66 @@ describe SimpleCovLinterFormatter::TextLinesFilter do
     let(:expected_result) { [] }
 
     it { expect(filter).to eq(expected_result) }
+  end
+
+  context "with reviewdog not installed" do
+    let(:use_reviewdog) { false }
+
+    let(:lines) do
+      %w{
+        Gemfile
+        Gemfile.lock
+        app/controllers/home_controller.rb
+        app/models/organization.rb
+        app/models/team_member.rb
+        app/models/team_responsibility.rb
+        app/models/user.rb
+        config/sidekiq.yml
+        spec/models/organization_spec.rb
+        spec/models/team_member_spec.rb
+        spec/models/user_spec.rb
+        spec/rails_helper.rb
+        spec/simplecov_config.rb
+        app/jobs/my_job.rb
+        spec/jobs/
+      }
+    end
+
+    let(:diff_result) do
+      <<~DOC
+        M Gemfile
+        M Gemfile.lock
+        D app/controllers/home_controller.rb
+        M app/models/user.rb
+        M config/sidekiq.yml
+        M spec/models/user_spec.rb
+        M spec/rails_helper.rb
+        ?? app/jobs/my_job.rb
+        ?? spec/jobs/
+      DOC
+    end
+
+    let(:expected_result) do
+      %w{
+        Gemfile
+        Gemfile.lock
+        app/controllers/home_controller.rb
+        app/models/user.rb
+        config/sidekiq.yml
+        spec/models/user_spec.rb
+        spec/rails_helper.rb
+        app/jobs/my_job.rb
+      }
+    end
+
+    it { expect(filter).to eq(expected_result) }
+
+    it do
+      expect_any_instance_of(described_class).to receive(:`)
+        .with("git status --porcelain")
+        .once
+
+      filter
+    end
   end
 end
